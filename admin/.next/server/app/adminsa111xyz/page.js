07b909f80e3a8a7f92826276777fb73a9789f5db
Mr Ali Raza <info.rana.aliraza@gmail.com>
2025-12-29 16:49:04 +0500

    new

--- admin/.next/server/app/adminsa111xyz/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/adminsa111xyz/page.js";
-const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_4781bc._.js");
-runtime.loadChunk("server/chunks/ssr/_b59cda._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/adminsa111xyz/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js?page=/adminsa111xyz/page { COMPONENT_0 => \"[project]/src/app/layout.js [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_1 => \"[project]/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_2 => \"[project]/src/app/adminsa111xyz/page.js [app-rsc] (ecmascript, Next.js server component)\", METADATA_3 => \"[project]/src/app/icon.png.mjs { IMAGE => \\\"[project]/src/app/icon.png [app-rsc] (static)\\\" } [app-rsc] (structured image object, ecmascript)\" } [app-rsc] (ecmascript) <facade>", CHUNK_PUBLIC_PATH).exports;
--- admin/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KqrY9Ri+7I7ELDybDbiY0SnZh9XJXgbm/FHCjc04ByY=\"\n}"
