ee441312073b73f95701479ca76f075f7a35ffba
Mr Ali Raza <info.rana.aliraza@gmail.com>
2026-01-31 10:36:01 +0500

    new

--- Zuroona_new/api/src/config/config.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./jeena-e9c10-firebase-adminsdk-2osts-63207acc62.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
